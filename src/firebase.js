2e6c43c235c5098cf7773f91fc79d21b5bc7ff84
niloy1513991 <niloy15-13991@diu.edu.bd>
2025-03-11 18:44:59 +0600

    Add Firebase configuration to .env file and update firebase.js to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhDbpxxtpjE8lNGZemCX5snD0yS2sUg_s",
-  authDomain: "netflixclone-d9cff.firebaseapp.com",
-  projectId: "netflixclone-d9cff",
-  storageBucket: "netflixclone-d9cff.firebasestorage.app",
-  messagingSenderId: "513235095340",
-  appId: "1:513235095340:web:755c0de296dbe995a0a689",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
